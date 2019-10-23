d1ce6d9282ee70ddf32be047ae9a344731f1922e
Arish-Shah <arish0246@gmail.com>
2019-10-24 00:46:09 +0530

    Made Topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import AddNote from './components/AddNote/AddNote'
+
+import Topbar from './components/Topbar/Topbar.jsx'
+import AddNote from './components/AddNote/AddNote.jsx'
 
 const App = () => {
 	return (
 		<div className="App">
+			<Topbar />
 			<AddNote />
 		</div>
 	)
