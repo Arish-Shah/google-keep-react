28d9c0ea82e337d54fa278ddc587c7a12d2b82cb
Arish-Shah <arish0246@gmail.com>
2019-12-29 21:53:10 +0530

    Updates

---
+++ precache-manifest.a5cb007c153e7da3a7125b1d15b2d4b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb4538c4111b1a3036b9dafba1d80fb",
+    "url": "/google-keep-react/index.html"
+  },
+  {
+    "revision": "f4d493e84d5bc5c39691",
+    "url": "/google-keep-react/static/js/2.b418524f.chunk.js"
+  },
+  {
+    "revision": "67b53bd88002cbaab924",
+    "url": "/google-keep-react/static/js/main.7eac422b.chunk.js"
+  },
+  {
+    "revision": "717bfa3154f12a4f2064",
+    "url": "/google-keep-react/static/js/runtime-main.f74cb802.js"
+  },
+  {
+    "revision": "c24b6b9c0fcd84c7b258879880472660",
+    "url": "/google-keep-react/static/media/firebase.c24b6b9c.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/google-keep-react/static/media/react.47ce6e77.png"
+  }
+]);
